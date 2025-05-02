4ede97c0d4809b858a3dff7d50cd99544257b72b
codify4 <kushta.joni@gmail.com>
2025-05-02 09:06:06 +0200

    fix: landing redirect

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,7 +40,9 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/signin') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/')
+
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
